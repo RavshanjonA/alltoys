394bb5b73b6b9c8c8c6ffdfe38480218da8ada87
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-02-23 01:31:45 +0500

    add template to toys app

--- toys/views.py
+++ toys/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Hello World </h1>')
+    return render(request,'toys/index.html',context={'welcome_text':"Welcome to Home Page"})
