dd2ac09313822b950ee6da925c6a0620963e408e
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-02-23 21:48:30 +0500

    add objects manager to user model

--- toys/models.py
+++ toys/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 
+class ActiveObjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class User(models.Model):
+    is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
+
+    object = models.Manager()
+    active_objects = ActiveObjectsManager()
+
+    def __str__(self):
+        return self.first_name
+
+class Toy(models.Model):
+    name= models.CharField(max_length=100)
+
