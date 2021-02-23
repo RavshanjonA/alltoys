9fa4e3a746ab355c8d8a57230184ff5189343371
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-02-23 22:31:23 +0500

    create tag and toy models

--- toys/models.py
+++ toys/models.py
@@ -9,7 +9,9 @@ class ActiveObjectsManager(models.Manager):
 class User(models.Model):
     is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
     age = models.IntegerField()
 
     objects = models.Manager()
@@ -19,9 +21,13 @@ class User(models.Model):
         return self.first_name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
 
 class Toy(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, related_name='toys', on_delete=models.CASCADE, null=True, blank=True)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
+    tag = models.ManyToManyField(Tag, related_name='toys')
