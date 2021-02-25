fc29391134c6839203d11814c5f0dd5f475bce23
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-02-25 10:47:39 +0500

    create toys and users list page

--- toys/urls.py
+++ toys/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, toys, users
+
 
 urlpatterns = [
     path('', home, name='home'),
+    path('toys/', toys, name='toys'),
+    path('users/', users, name='users'),
+
 ]
--- toys/views.py
+++ toys/views.py
@@ -5,7 +5,17 @@ from .models import Toy, User
 
 
 def home(request):
+
+    return render(request, 'toys/home.html', context={'text': 'Hello !\nWelcome to HomePage'})
+
+
+def toys(request):
     toys = Toy.objects.all()
+
+    return render(request, 'toys/index.html', context={'toys': toys})
+
+
+def users(request):
     users = User.objects.all()
 
-    return render(request, 'toys/index.html', context={'users': users, 'toys': toys})
+    return render(request, 'toys/member.html', context={'users': users})
