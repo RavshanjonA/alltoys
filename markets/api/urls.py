2a0d5c3e14c153a302c4074876064dcf5eae003b
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-03-04 01:45:03 +0500

    add generic class view for product of markets

--- markets/api/urls.py
+++ markets/api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 
 from markets import views
-from markets.views import ProductView
+from markets.views import ProductView, ProductListView, ProductItemView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = 'markets'
 urlpatterns = [
     path('show/', views.all_markets, name='all_markets'),
     path('show/<int:id>', views.index_market, name='index_markets'),
-    path('template/', ProductView.as_view(), name='template')
+    path('template/', ProductView.as_view(), name='template'),
+    path('product/', ProductListView.as_view(), name='product'),
+    path('product/create', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>', ProductItemView.as_view(), name='product_item'),
+    path('product/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- markets/views.py
+++ markets/views.py
@@ -1,15 +1,51 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 
-from markets.models import Market
+from markets.models import Market, Product
+
 
 class ProductView(TemplateView):
     template_name = 'markets/template.html'
     extra_context = {'source': 'Product Page'}
 
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product/index.html'
+
+    def get_queryset(self):
+        product = Product.objects.all()
+        product.order_by('-name')
+        return product
+
+
+class ProductItemView(DetailView):
+    model = Product
+    template_name = 'product/item.html'
+
+
+class ProductDeleteView(DetailView):
+    model = Product
+    template_name = 'product/delete.html'
+    success_url = reverse_lazy('markets:product ')
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'price', 'descirption']
+    template_name = 'product/create.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'price', 'descirption']
+    template_name = 'product/update.html'
+
+
 def all_markets(request):
-    markets = Market.objects.all()
+    markets = Market.objects.all().order_by('id')
     my_ob = {
         'markets': markets
     }
