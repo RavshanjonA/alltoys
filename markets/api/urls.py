877012e71fef8d075684d983ae683c9982b8a9cd
Ahmadjonov-98 <akhmadjonovr.98@gamil.com>
2021-03-03 11:13:47 +0500

    add templates to markets app

--- markets/api/urls.py
+++ markets/api/urls.py
@@ -5,6 +5,7 @@ from markets import views
 app_name = 'markets'
 urlpatterns = [
     path('', views.index, name='market'),
-
+    path('show/', views.all_markets, name='all_markets'),
+    path('show/<int:id>', views.index_market, name='index_markets'),
 
 ]
--- markets/views.py
+++ markets/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("<h1> Hello World </h1>")
+from markets.models import Market
 
+
+def index(request):
+    return HttpResponse("<h1> Hello this is Market place</h1>")
+
+
+def all_markets(request):
+    markets = Market.objects.all()
+    my_ob = {
+        'markets': markets
+    }
+    return render(request, 'markets/index.html', context=my_ob)
+
+
+def index_market(request, **kwargs):
+    market = Market.objects.get(pk=kwargs.get('id'))
+    my_ob = {
+        'market': market
+    }
+    return render(request, 'markets/a_market.html', context=my_ob)
